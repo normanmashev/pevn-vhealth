2e413602865b36931b0bfdbeba2be858794874e9
Nariman Imashev <imashnariman@gmail.com>
2021-12-05 12:01:58 +0600

    upd: move values to env

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const { Pool } = require("pg");
 
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+
 const pool = new Pool({
-	user: "postgres",
-	password: "admin",
-	port: 5433,
-	host: "localhost",
-	database: "hw2",
+	user: DB_USER,
+	password: DB_PASSWORD,
+	port: DB_PORT,
+	host: DB_HOST,
+	database: DB_NAME,
 });
 
 module.exports = pool;
